20ee51aa35742cb5a59a3c5033cf37d7eaea744f
Polygon65 <99875965+Polygon65@users.noreply.github.com>
2023-06-09 14:25:28 +0700

    Update config.bash

--- Watch/config.bash
+++ Watch/config.bash
@@ -1,4 +1,4 @@
-download="https://sfile.mobi/fRvZHdGe7Q7"
+download="https://sfile.mobi/ldWFxDYeju7"
 token="ewogICJrZXkiOiJHcmVlbiIKfQp7CiAgImRhdGEiOiI3MDY1NmE3NTYxNmU2NzZiNjU2ZTc0NjE2ZTY3NmE2MTc5NjFBIiwKICAidmFsdWUiOiJ0cnVlIgp9.QXBhIGx1IGVudG9kZCAK"
-link="https://khaddavi.net/jgiqQfpX"
+link="https://khaddavi.net/BYPcDlR"
 Whatsapp="IJqVQWOyBqX8BmNCu6Amhn"
